6f342c96ff645380d037f0b28ab8a146fd29f459
PrideOfConcept <info@PrideOfConcept.com>
2018-03-15 19:44:07 -0400

    Header: styling improvments + images

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,16 @@ import '../styles/header.scss';
 // Since this component is simple and static, there's no parent container for it.
 const AboutPage = () => {
   return (
-    <header>
-      Header Here
+    <header className={'row justify-content-md-center'}>
+      <div className={'col'}>
+        &nbsp;
+      </div>
+      <div className={'col-6 text-center'}>
+        <h2>Lunch Tyme</h2>
+      </div>
+      <div className={'col text-right'}>
+        <img src={'./styles/assets/icon_map@2x.png'} width="60px"/>
+      </div>
     </header>
   );
 };
