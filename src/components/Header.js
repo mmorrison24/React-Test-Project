8b946170e2e0f2426c100c7e60d5586b19125f85
PrideOfConcept <info@PrideOfConcept.com>
2018-03-15 20:11:27 -0400

    Header: tweaking styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import '../styles/header.scss';
 // Since this component is simple and static, there's no parent container for it.
 const AboutPage = () => {
   return (
-    <header className={'row justify-content-md-center'}>
+    <header className={'row justify-content-md-center align-items-end'}>
       <div className={'col'}>
         &nbsp;
       </div>
@@ -12,7 +12,7 @@ const AboutPage = () => {
         <h2>Lunch Tyme</h2>
       </div>
       <div className={'col text-right'}>
-        <img src={'./styles/assets/icon_map@2x.png'} width="60px"/>
+        <img src={'./styles/assets/icon_map@2x.png'} width="40px"/>
       </div>
     </header>
   );
